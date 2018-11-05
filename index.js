4fc3fa10796201471609921cb6b48badf50887cc
Jasper Zonneveld <jasper@swis.nl>
2018-11-05 16:00:44 +0100

    Do not subtract size from x position of marker

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ module.exports = function leafletImage(map, callback) {
     // tiles, paths, and then markers
     map.eachLayer(drawTileLayer);
     map.eachLayer(drawEsriDynamicLayer);
-    
+
     if (map._pathRoot) {
         layerQueue.defer(handlePathRoot, map._pathRoot);
     } else if (map._panes) {
@@ -50,11 +50,11 @@ module.exports = function leafletImage(map, callback) {
             layerQueue.defer(handleMarkerLayer, l);
         }
     }
-    
+
     function drawEsriDynamicLayer(l) {
         if (!L.esri) return;
-       
-        if (l instanceof L.esri.DynamicMapLayer) {                       
+
+        if (l instanceof L.esri.DynamicMapLayer) {
             layerQueue.defer(handleEsriDymamicLayer, l);
         }
     }
@@ -210,7 +210,7 @@ module.exports = function leafletImage(map, callback) {
 
         if (size instanceof L.Point) size = [size.x, size.y];
 
-        var x = Math.round(pos.x - size[0] + anchor.x),
+        var x = Math.round(pos.x - anchor.x),
             y = Math.round(pos.y - anchor.y);
 
         canvas.width = dimensions.x;
@@ -228,18 +228,18 @@ module.exports = function leafletImage(map, callback) {
 
         if (isBase64) im.onload();
     }
-    
+
     function handleEsriDymamicLayer(dynamicLayer, callback) {
         var canvas = document.createElement('canvas');
         canvas.width = dimensions.x;
         canvas.height = dimensions.y;
-    
+
         var ctx = canvas.getContext('2d');
-    
+
         var im = new Image();
         im.crossOrigin = '';
         im.src = addCacheString(dynamicLayer._currentImage._image.src);
-    
+
         im.onload = function() {
             ctx.drawImage(im, 0, 0);
             callback(null, {
